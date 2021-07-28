2dfe1a57bfbad809215e637202fbf89197c6f57e
Aleksey <otomivetra@mail.ru>
2021-07-28 13:12:48 +0300

    task 1_3

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const User = require('./model/User');
 const app = express();
 const PORT = 3001;
 
 app.get('/', (req, res) => {
    res.send('Server Index Page');
+});
+app.get('/users', (req, res) => {
+   res.json({
+      items: User.list()
+   });
+});
+
+app.get('/users/:id', (req, res) => {
+   res.json({
+      item: User.getById(+req.params.id)
+   });
 });
 
 
