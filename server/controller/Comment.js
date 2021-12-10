1035732349a7f9c9372ce3d2fdd2a9d6e9b74769
Aleksey <otomivetra@mail.ru>
2021-12-10 12:42:26 +0300

    task 4_1

--- server/controller/Comment.js
+++ server/controller/Comment.js
@@ -1,18 +1,26 @@
 const Comment = require('../model/mongo/Comment');
 const list = async (req, res) => {
-  const postId = req.query.postId
-  const { skip = 0, limit = 10 } = req.query;
-  const criteria = {}
-  if (postId) { criteria.postId = +postId }
-  res.json({
-    count: await Comment.countDocuments(criteria),
-    items: await Comment.find(criteria).skip(+skip).limit(+limit)
-  });
+  try {
+    const postId = req.query.postId
+    const { skip = 0, limit = 10 } = req.query;
+    const criteria = {}
+    if (postId) { criteria.postId = +postId }
+    res.json({
+      count: await Comment.countDocuments(criteria),
+      items: await Comment.find(criteria).skip(+skip).limit(+limit)
+    });
+  } catch (error) {
+    next(error)
+  }
 }
 const getById = async (req, res) => {
-  res.json({
-    item: await Comment.findOne({ id: +req.params.id })
-  })
+  try {
+    res.json({
+      item: await Comment.findOne({ id: +req.params.id })
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = {
--- server/controller/Post.js
+++ server/controller/Post.js
@@ -1,18 +1,26 @@
 const Post = require('../model/mongo/Post');
 const list = async (req, res) => {
-  const userId = req.query.userId
-  const { skip = 0, limit = 10 } = req.query;
-  const criteria = {};
-  if (userId) { criteria.userId = +userId };
-  res.json({
-    count: await Post.countDocuments(criteria),
-    items: await Post.find(criteria).skip(+skip).limit(+limit)
-  });
+  try {
+    const userId = req.query.userId
+    const { skip = 0, limit = 10 } = req.query;
+    const criteria = {};
+    if (userId) { criteria.userId = +userId };
+    res.json({
+      count: await Post.countDocuments(criteria),
+      items: await Post.find(criteria).skip(+skip).limit(+limit)
+    });
+  } catch (error) {
+    next(error)
+  }
 }
 const getById = async (req, res) => {
-  res.json({
-    item: await Post.findOne({ id: +req.params.id })
-  })
+  try {
+    res.json({
+      item: await Post.findOne({ id: +req.params.id })
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 module.exports = {
   list,
